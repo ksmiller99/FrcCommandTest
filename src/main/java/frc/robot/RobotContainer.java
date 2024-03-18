ee6b4d56ef2675306333259dc5cb839af2f38f5a
Kevin S. Miller <ksmiller99@users.noreply.github.com>
2024-03-17 22:39:56 -0400

    initial commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  private final CommandXboxController m_driverController = new CommandXboxController(
+      OperatorConstants.kDriverControllerPort);
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+  }
+
+  private void configureBindings() {
+    m_driverController.b()
+    .whileTrue(
+        m_exampleSubsystem.runOnce(() -> m_exampleSubsystem.Print("before"))
+        //.andThen(()->new WaitCommand(3)) //does not wait
+        //.andThen(new WaitCommand(3))     //next command does not run
+        //.andThen(WaitCommand(3))         //"WaitCommand(int) is undefined for the type RobotContainer"
+        .andThen(m_exampleSubsystem.runOnce(() -> m_exampleSubsystem.Print("after")))
+    );
+  }
+
+}
